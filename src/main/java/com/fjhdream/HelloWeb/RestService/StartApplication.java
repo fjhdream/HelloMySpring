9c67025de72cac29670b60a9c2ea17ead20d31a1
fjhdream <fjh_dream@163.com>
2020-06-10 23:52:08 +0800

    SimpleRestService,/Greeting

---
+++ src/main/java/com/fjhdream/HelloWeb/RestService/StartApplication.java
@@ -0,0 +1,13 @@
+package com.fjhdream.HelloWeb.RestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class);
+    }
+}
